bd042a5a518920bc9121378cc2ad7c854edc70a5
antonnyst <35500074+antonnyst@users.noreply.github.com>
2023-08-29 14:13:38 +0200

    Implement accepting rules

--- README.md
+++ README.md
@@ -3,25 +3,6 @@ Because rust is fun.
 
 ## Features
 - Accepted role by reaction
-- Courseroom commands
 
 ## Commands
-- role: Join courserooms
-- courseroom: Administer courseroom roles and discord channels
 - rules: Administer reaction rules roles
-
-Saves channel and role ids in an Postgres database
-
-```
-table! {
-    Guilds {
-        rules_channel_id,
-        rules_accepted_role_id,
-        Courserooms {
-            name,
-            channel_id,
-            role_id
-        }
-    }
-}
-```
--- src/commands/rules.rs
+++ src/commands/rules.rs
@@ -1,5 +1,5 @@
+use serenity::framework::standard::{Args, CommandResult};
 use serenity::framework::standard::macros::command;
-use serenity::framework::standard::{CommandResult, Args};
 use serenity::model::prelude::*;
 use serenity::{prelude::*, async_trait};
 use serde::{Deserialize, Serialize};
@@ -7,18 +7,26 @@ use std::fs;
 
 #[derive(Serialize, Deserialize)]
 pub struct RulesConfig {
-    channel_id: u64,
-    rules_role_id: u64
+    rules_role_id: Option<RoleId>
 }
 
-fn read_config(path: &str) -> Result<Config, Box<dyn std::error::Error>> {
-    let data = fs::read(path)?;
-    let text = String::from_utf8(data)?;
-    let config: Config = toml::from_str(&text)?;
-    Ok(config)
+const RULES_CONFIG_PATH: &str = "rules_config.toml";
+
+fn read_config(path: &str) -> Result<RulesConfig, Box<dyn std::error::Error>> {    
+    if let Ok(data) = fs::read(path) {
+        let text = String::from_utf8(data)?;
+        let config: RulesConfig = toml::from_str(&text)?;
+        Ok(config)
+    } else {
+        Ok(
+            RulesConfig {
+                rules_role_id: None
+            }
+        )
+    }
 }
 
-fn write_config(config: &Config, path: &str) -> Result<(), Box<dyn std::error::Error>> {
+fn write_config(config: &RulesConfig, path: &str) -> Result<(), Box<dyn std::error::Error>> {
     let text = toml::to_string(config)?;
     std::fs::write(path, text)?;
     Ok(())
@@ -32,18 +40,19 @@ async fn rules(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
         msg.reply(&ctx, "Wrong usage!").await?;
     };
 
-    if let Some(guild) = msg.guild(&ctx) {
+    if let Ok(guild) = Guild::get(&ctx, msg.guild_id.unwrap()).await  {
         match args.single_quoted::<String>() {
             Ok(x) => {
                 match x.as_str() {
-                    "channel" => {
-                        let channel_name = args.rest();
-                        if let Some(channel) = guild.channel_id_from_name(&ctx, channel_name) {
-                            
-                        }
-                    },
                     "role" => {
-    
+                        let role_name = args.rest();
+                        if let Some(role) = guild.role_by_name(role_name) {
+                            if let Ok(mut config) = read_config(RULES_CONFIG_PATH) {
+                                config.rules_role_id = Some(role.id);
+                                write_config(&config, RULES_CONFIG_PATH)
+                                    .expect("Failed to write rules config");
+                            }
+                        } 
                     },
                     _ => {}
                 }
@@ -58,23 +67,27 @@ async fn rules(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
     Ok(())
 }
 
-const RULES_CHANNEL_ID: u64 = 1110930405667905596;
-const ACCEPTED_RULES_ROLE_ID: u64 = 1110930504313733281;
-
 pub struct RulesHandler;
 
 #[async_trait]
 impl EventHandler for RulesHandler {
     async fn reaction_add(&self, ctx: Context, reaction: Reaction) {
         println!("Handling an reaction_add");
-        if reaction.channel_id.0 == RULES_CHANNEL_ID {
-            if let Some(guild_id) = reaction.guild_id {
+        if let Ok(guild) = Guild::get(&ctx, reaction.guild_id.unwrap()).await  {
+            println!("Found guild");
+
+            let rules_channel_id = guild.rules_channel_id.expect("Could not find rules channel");
+            let role = read_config(RULES_CONFIG_PATH).unwrap().rules_role_id.expect("Could not get role id");
+
+            if reaction.channel_id.eq(&rules_channel_id) {
                 if let Some(user_id) = reaction.user_id {
-                    if let Ok(mut member) = guild_id.member(&ctx, user_id).await {
-                        member.add_role(&ctx, ACCEPTED_RULES_ROLE_ID).await.unwrap();
+                    if let Ok(mut member) = guild.member(&ctx, user_id).await {
+                        member.add_role(&ctx, role).await.unwrap();
+                        println!("Gave {} the role {}", member.display_name(), role.to_string())
                     }
                 }
             }
+
         }
     }
 }
